d8942c6322386178efa3fef7838add420268cb81
Siri Høyås <siri@skjervold.me>
2023-03-06 14:31:12 +0100

    fixed footer link

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <h2>Tootsie</h2>
+      <h2>Tootsie 🦶🏻</h2>
     </footer>
   );
 }
