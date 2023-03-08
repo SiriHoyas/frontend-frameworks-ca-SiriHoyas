37576d2e3bd56c19f116b48ce5ec437215ebf3ea
Siri Høyås <siri@skjervold.me>
2023-03-08 21:56:48 +0100

    adds hook for calling API

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,10 @@
 import ProductCard from "../components/ProductCard/ProductCard";
+import useApi from "../hooks/UseApi";
 import styles from "./HomePage.module.css";
 
 function HomePage() {
+  const { data, isLoading, isError } = useApi("https://jsonplaceholder.typicode.com/todos");
+  console.log(data);
   return (
     <main className={styles.homepageMain}>
       <section className={styles.productsContainer}>
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./CartSlice";
+
 export type RootState = ReturnType<typeof store.getState>;
 
 export const store = configureStore({
