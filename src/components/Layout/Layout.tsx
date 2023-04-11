b5075dfdf2cb3c2166354b389a04e27a441ee390
Siri Høyås <siri@skjervold.me>
2023-04-11 14:15:18 +0200

    ?

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,7 @@
 import { Outlet, Route } from "react-router-dom";
 
 import Footer from "./Footer";
-import Header from "./Header";
+import Header from "../../components/Layout/Header";
 
 function Layout() {
   return (
